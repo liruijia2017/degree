fe9003c3ce56e15b8ea04b0f14029f80029a1e78
liruijia2017 <1142698760@qq.com>
2022-12-26 19:19:06 +0800

    Update main.m

--- 3/main.m
+++ 3/main.m
@@ -1,6 +1,6 @@
 tic; clear; close all; clc; addpath(genpath(pwd));   
-load aggregation 
-N = round(sqrt(length(Y))); tau = 0.6; i = 2; c = 7;
+load flame 
+N = round(sqrt(length(Y))); tau = 0.6; i = 2; c = 2;
 [LabelGroup,ExistGroup,centers,radius,numG] = Group(X,i*N); 
 [DisGroup,LD,SD,WkNN] = Denseness(X,LabelGroup,ExistGroup,centers,radius,numG,10); 
 GroupID = LabelnonOutlierGroup(c,LD,SD,WkNN,tau,numG);
