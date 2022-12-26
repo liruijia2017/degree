ba663829f27a9dd29ad3dec4295be2efefde2cf7
liruijia2017 <1142698760@qq.com>
2022-12-26 19:17:27 +0800

    Update main.m

--- 2/main.m
+++ 2/main.m
@@ -1,6 +1,6 @@
 tic; clear; close all; clc; addpath(genpath(pwd));   
-load aggregation 
-k = 10; tau = 0.6; c = 7; 
+load flame 
+k = 10; tau = 0.6; c = 2; 
 D = EuclideanD(X,X);
 [~,D_ID] = sort(D,2); 
 [~,~,W] = SparseGraph(D,D_ID,k); 
