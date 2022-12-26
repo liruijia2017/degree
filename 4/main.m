15527719c5680e5ec1387af5d2ea6690a4f25c46
liruijia2017 <1142698760@qq.com>
2022-12-26 19:19:42 +0800

    Update main.m

--- 4/main.m
+++ 4/main.m
@@ -1,6 +1,6 @@
 tic; clear; close all; clc; addpath(genpath(pwd));   
-load aggregation 
-k = 30; eps = 0;
+load flame 
+k = 50; eps = 0;
 [Distance,matV,matID] = SortDistance(X,X); 
 kNN = SparseGraph(Distance,matID,k); 
 [D,T,r] = DivideData(kNN,matV,k,eps);
